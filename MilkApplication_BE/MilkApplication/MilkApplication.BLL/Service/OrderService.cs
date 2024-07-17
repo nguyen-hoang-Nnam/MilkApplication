c1600aa29a2dfd0c7da6733800469e184b9e29e7
Trung <wowholyshit07@gmail.com>
2024-07-17 19:31:50 +0700

    update

--- MilkApplication_BE/MilkApplication/MilkApplication.BLL/Service/OrderService.cs
+++ MilkApplication_BE/MilkApplication/MilkApplication.BLL/Service/OrderService.cs
@@ -203,7 +203,7 @@ namespace MilkApplication.BLL.Service
                 return new ResponseDTO
                 {
                     IsSucceed = true,
-                    Message = $"{staff.FullName} updated order status successfully.",
+                    Message = $"Staff {staff.FullName} updated order status successfully.",
                     Data = updatedOrderDTO
                 };
             }
